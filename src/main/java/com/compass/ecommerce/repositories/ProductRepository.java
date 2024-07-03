40ac41e03a50a319766830e389a19c991e16e239
Natan Neves <dev.natanbarbosa@gmail.com>
2024-07-03 02:20:33 -0300

    feat: creating product repository

---
+++ src/main/java/com/compass/ecommerce/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.compass.ecommerce.repositories;
+
+import com.compass.ecommerce.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
