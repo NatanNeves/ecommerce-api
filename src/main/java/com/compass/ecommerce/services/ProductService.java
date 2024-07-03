b65aac1e7db81f4b1d1f68b3f2a9310284332017
Natan Neves <dev.natanbarbosa@gmail.com>
2024-07-03 14:00:15 -0300

    feat: add functions in product service

--- src/main/java/com/compass/ecommerce/services/ProductService.java
+++ src/main/java/com/compass/ecommerce/services/ProductService.java
@@ -7,6 +7,8 @@ import com.compass.ecommerce.repositories.StockRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ProductService {
 
@@ -17,15 +19,61 @@ public class ProductService {
     private StockRepository stockRepository;
 
 
-    public Product createProduct(ProductDTO data){
-       //fazer tratamento de possiveis excessões
+    public Optional<Product> findByName(String name){
+        return productRepository.findByName(name);
+    }
 
-        Product prod = new Product();
-        prod.setName(data.name());
-        prod.setDescription(data.description());
-        prod.setPrice(data.price());
-        prod.setQuantity(data.quantity());
+    public Product createProduct(ProductDTO data) throws Exception {
+        if (productRepository.findByName(data.name()).isEmpty()) {
+            this.priceValidate(data.price());
 
-        return prod;
+            Product prod = new Product();
+            prod.setName(data.name());
+            prod.setDescription(data.description());
+            prod.setPrice(data.price());
+            prod.setQuantity(data.quantity());
+            return productRepository.save(prod);
+
+        } else {
+            throw new Exception("este produto já existe");
+        }
+    }
+
+
+    public Product updateProduct(Long id, ProductDTO data) throws Exception {
+        Optional<Product> existingProductOpt = productRepository.findById(id);
+        this.existingProductValidate(id);
+
+        Product existingProduct = existingProductOpt.get();
+        existingProduct.setName(data.name());
+        existingProduct.setDescription(data.description());
+        existingProduct.setPrice(data.price());
+        existingProduct.setQuantity(data.quantity());
+
+        this.priceValidate(data.price());
+        return productRepository.save(existingProduct);
+    }
+
+    public void priceValidate(Double price) throws Exception {
+        if (price == null || price <= 0) {
+            throw new Exception("O preço deve ser positivo e não pode ser nulo");
+        }
+    }
+
+    public void deleteProduct(Long id) throws Exception {
+        Optional<Product> existingProductOpt = productRepository.findById(id);
+        this.existingProductValidate(id);
+
+        Product existingProduct = existingProductOpt.get();
+        productRepository.delete(existingProduct);
+    }
+
+    public Product existingProductValidate(Long id) throws Exception {
+        Optional<Product> existingProductOpt = productRepository.findById(id);
+
+        if (existingProductOpt.isEmpty()) {
+            throw new Exception("ID de produto inválido");
+        }
+        return existingProductOpt.get();
     }
 }
