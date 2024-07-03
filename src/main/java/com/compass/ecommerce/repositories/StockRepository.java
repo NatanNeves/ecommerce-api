d9917abf709216e1a593a24f9c7ed8ca46b6b91b
Natan Neves <dev.natanbarbosa@gmail.com>
2024-07-03 02:21:02 -0300

    feat: creating stock repository

---
+++ src/main/java/com/compass/ecommerce/repositories/StockRepository.java
@@ -0,0 +1,9 @@
+package com.compass.ecommerce.repositories;
+
+import com.compass.ecommerce.domain.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockRepository extends JpaRepository<Stock, Long> {
+}
