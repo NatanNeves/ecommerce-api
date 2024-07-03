ad9dbf2f7a0794147ca6761bf2ed31bdc89481de
Natan Neves <dev.natanbarbosa@gmail.com>
2024-07-03 01:42:35 -0300

    feat: creating product entity

---
+++ src/main/java/com/compass/ecommerce/domain/Product.java
@@ -0,0 +1,36 @@
+package com.compass.ecommerce.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Product implements Serializable {
+    private static final long SerialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Boolean available;
+
+    @ManyToOne
+    @JoinColumn(name = "stock_id")
+    private Stock stock;
+}
