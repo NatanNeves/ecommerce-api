999be72d7175bd7e9bb47b4832ed0f54e848c923
Natan Neves <dev.natanbarbosa@gmail.com>
2024-07-05 01:23:47 -0300

    feat: creating associate key "itempk"

---
+++ src/main/java/com/compass/ecommerce/domain/ItemPK.java
@@ -0,0 +1,25 @@
+package com.compass.ecommerce.domain;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Embeddable
+@Getter
+@Setter
+@EqualsAndHashCode(of = {"product", "sale"})
+public class ItemPK implements Serializable {
+    private static final long SerialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    @ManyToOne
+    @JoinColumn(name = "sale_id")
+    private Sale sale;
+}
