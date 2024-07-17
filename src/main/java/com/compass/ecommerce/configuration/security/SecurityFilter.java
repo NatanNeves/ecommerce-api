c7a107cfa9d0261a7bdc61cbdd86bd378c945870
Natan Neves <dev.natanbarbosa@gmail.com>
2024-07-17 12:46:57 -0300

    feat: correction on internal filter

--- src/main/java/com/compass/ecommerce/configuration/security/SecurityFilter.java
+++ src/main/java/com/compass/ecommerce/configuration/security/SecurityFilter.java
@@ -16,27 +16,37 @@ import java.io.IOException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
     @Autowired
     private TokenService tokenService;
+
     @Autowired
     private UserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var token = this.recoverToken(request);
-        if(token != null){
+        var token = recoverToken(request);
+
+        if (token != null) {
             var login = tokenService.validateToken(token);
             UserDetails user = userRepository.findByLogin(login);
 
-            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            if (user != null) {
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                // Handle case where user is not found
+                // You may choose to log an error or handle this case based on your application's requirements
+                System.out.println("User not found for login: " + login);
+            }
         }
+
         filterChain.doFilter(request, response);
     }
 
-    private String recoverToken(HttpServletRequest request){
+    private String recoverToken(HttpServletRequest request) {
         var authHeader = request.getHeader("Authorization");
-        if(authHeader == null) return null;
+        if (authHeader == null) return null;
         return authHeader.replace("Bearer ", "");
     }
 }
